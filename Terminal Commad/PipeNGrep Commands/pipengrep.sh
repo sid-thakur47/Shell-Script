77f5717613afc27c1ed23c0ebc2bba2c59d0763b
sid-thakur47 <sidthakur258@gmail.com>
2020-03-24 18:20:43 +0530

    ADDED:PipeNGrep Commands

---
+++ Terminal Commad/PipeNGrep Commands/pipengrep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -xcd 
+
+
+	find . -size +5M
+    find . -size +5M | grep pdf
+    find . -name data*
+    grep CAPTAIN linux-content/data.csv
+    cd ..
+    grep CAPTAIN linux-content/data.csv
+    grep -r CAPTAIN
+    ls -p | grep /
+    ls -p | grep -v /
+    env
+    history
