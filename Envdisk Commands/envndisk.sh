5582e9d7744a578ee838a92db4ccb9a26a61f730
sid-thakur47 <sidthakur258@gmail.com>
2020-03-24 18:08:34 +0530

    ADDED: Envdisk Commands

---
+++ Envdisk Commands/envndisk.sh
@@ -0,0 +1,15 @@
+#!bin/bash -xcdcd
+
+
+	echo $USER
+    echo $SHELL
+    echo $HOME
+    cd ..
+    du -sh linux-content/
+    du -sm * | sort -nr
+    du -sk * | sort -n
+	find ~/siddhesh2 -name siddhesh.txt
+    find ~/siddhesh2 -name siddhesh.txt
+    find . -type f -empty
+    clear
+   
