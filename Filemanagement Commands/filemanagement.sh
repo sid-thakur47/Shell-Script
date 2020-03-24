2576254eb28279501d0fea5f2f6f3675b55b5cf9
sid-thakur47 <sidthakur258@gmail.com>
2020-03-24 18:10:41 +0530

    ADDED:File Management Commands

---
+++ Filemanagement Commands/filemanagement.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+pwd
+ls .
+ls ..
+ls ~
+ls -a
+ls -l
+mkdir temp2
+cd temp1
+ls
+touch test
+ls -p
+cp test test1
+ls -p
+mv test1 test2
+ls -p
+ln -s test temp3
+ls -p temp3
+ln -s linux_chit_sheet.pdf ~/QuickLink2/
---
+++ Filemanagement Commands/files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pwd
+ls .
+ls ..
+ls ~
+ls -a
+ls -l
+mkdir temp2
+cd temp1
+ls
+touch test
+ls -p
