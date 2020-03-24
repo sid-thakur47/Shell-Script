6585a1abd4518fed3973fe4a6d273b4e87a8719f
sid-thakur47 <sidthakur258@gmail.com>
2020-03-24 18:29:44 +0530

    ADDED:ArthemeticOpp  to understand the precedence of the operators

---
+++ ProgrammingConcepts/ArthemeticOpp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+   read -p "Enter First number:" a
+      read -p "Enter Secound Number" b
+         read -p "Enter Third Number" c
+         z=$(($a + ($b * $c)))
+         y=$(($c + ($a / $b)))
+         x=$((($a % $b) + $c ))
+         d=$((($a * $b) + $c))
+   echo $z $y $x $d
