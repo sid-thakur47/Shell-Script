00c45a2ea1e5b20ea9468fc282eb49eda5ef35aa
sid-thakur47 <62283575+sid-thakur47@users.noreply.github.com>
2020-03-26 19:28:30 +0530

    MODIFIED:PrimeFactors Corrected Indentation

--- Arrays/PrimeFactors.sh
+++ Arrays/PrimeFactors.sh
@@ -2,19 +2,19 @@ read -p "Enter a number: " number
 k=0
 for (( i=2; i<=$number; i++ ))
 	do
-		checkPrime=1
+	checkPrime=1
 		for (( j=2; j<i; j++ ))
 		do
 			if [ $(($i%$j)) -eq 0 ]
 			then
-				checkPrime=0
+			checkPrime=0
 			fi
 		done
 		if [ $checkPrime -eq 1 ] 
 		then
 			if [ $(($number % $i)) -eq 0 ]
 				then
-					factors[((k++))]=$i
+				factors[((k++))]=$i
 			fi
 		  fi
 	done
