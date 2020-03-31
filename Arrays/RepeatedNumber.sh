316961f2972fc52af4cfa9874ba8b00a2e9f9f3b
sid-thakur47 <sidthakur258@gmail.com>
2020-03-31 13:05:09 +0530

    MODIFIED:RepeatedNumber Changed function name

--- Arrays/RepeatedNumber.sh
+++ Arrays/RepeatedNumber.sh
@@ -1,23 +1,25 @@
-declare -a PrimeFactor
-findReverse() { 
-reverse=0 
-number=$1 
+declare -a repeated
+
+findRepeated() {
+repeate=0
+number=$1
 while [ $number -gt 0 ]
 	do
 		rem=$(( $number % 10 ))
-		reverse=$(( $reverse * 10 + $rem ))
+		repeate=$(( $repeate * 10 + $rem ))
 		number=$(( $number / 10 ))
 	done
-echo $reverse
+	echo $repeate
 }
+
 read -p "Enter ending number:" END
 for(( i=10;i<=$END;i++ ))
 do
-	reverse=$(findReverse $i)
-		if [[ $reverse -eq $i ]]
+	repeate=$(findRepeated $i)
+		if [[ $repeate -eq $i ]]
 			then
 			repeated[((k++))]=$i 
 		fi
 done
-echo "Repeated numbers are:" ${repeated[*]} 
+	echo "Repeated numbers are:" ${repeated[*]}
 
